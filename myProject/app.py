b21d34f4193b9096360e8e86c2191fc22e3bc96d
HarimJ <58238503+HarimJ@users.noreply.github.com>
2020-08-26 21:07:46 +0900

    db connect

--- myProject/app.py
+++ myProject/app.py
@@ -1,27 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.journey
+
+
 @app.route('/')
-def home():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+def home():
     return render_template('index.html')
 
-@app.route('/first.html')
+
+@app.route('/first.html', methods=['POST'])
 def my_page():
     return render_template('first.html')
+
+# 여러 다른 페이지에 내용을 한디비에 저장은 못하나?
+def write_diary():
+    first_receive = request.form['first']
+
+    review = {
+        'firstDiary': first_receive
+    }
+
+    db.reviews.insert_one(review)
+    return jsonify({'result': 'success', 'msg':'저장되었습니다.'})
 
 @app.route('/second.html')
 def my_page():
     return render_template('second.html')
 
+
 @app.route('/third.html')
 def my_page():
     return render_template('third.html')
 
+
 @app.route('/fourth.html')
 def my_page():
     return render_template('fourth.html')
-
 
 
 if __name__ == '__main__':
