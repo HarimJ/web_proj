de1b5af2ce833e0bcea1287db3acf650ffa26028
HarimJ <58238503+HarimJ@users.noreply.github.com>
2020-09-06 16:40:49 +0900

    something new

--- myProject/app.py
+++ myProject/app.py
@@ -11,51 +11,51 @@ db = client.journey
 def home():
     return render_template('index.html')
 
+@app.route('/fourth')
+def diary_page():
+    return render_template('fourth.html')
 
-@app.route('/diaries/<num>')
-def get_diary(num):
-    diary = db.reviews.find_one({'num': num})
-    return render_template('diary_' + num + '.html', diary=diary['content'])
-
-
-@app.route('/first', methods=['GET'])
-def my_page():
-    first_diary = db.reviews.find_one({'name': 'first'})
-    if first_diary is None:
-        return render_template('first.html')
-    else:
-        return render_template('first.html', first_diary=first_diary['content'])
-
-
-# 여러 다른 페이지에 내용을 한디비에 저장은 못하나?
-@app.route('/diary_1', methods=['POST'])
-def write_diary():
-    first_receive = request.form['first']
-    print(first_receive)
-
-    review = {
-        'name': 'first',
-        'content': first_receive
-    }
-
-    db.reviews.insert_one(review)
-    return jsonify({'result': 'success', 'msg': '저장되었습니다.'})
-
+@app.route('/first')
+def photo_page():
+    return render_template('first.html')
 
 @app.route('/second')
-def second_page():
+def list_page():
     return render_template('second.html')
-
-
-@app.route('/third')
-def third_page():
-    return render_template('third.html')
-
-
-@app.route('/fourth.html')
-def fourth_page():
-    return render_template('fourth.html')
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
+# @app.route('/diaries/<num>')
+# def get_diary(num):
+#     diary = db.reviews.find_one({'num': num})
+#     return render_template('diary_' + num + '.html', diary=diary['content'])
+#
+#
+# @app.route('/fourth', methods=['GET'])
+# def my_page():
+#     first_diary = db.reviews.find_one({'name': 'first'})
+#     if first_diary is None:
+#         return render_template('fourth.html')
+#     else:
+#         return render_template('fourth.html', first_diary=first_diary['content'])
+#
+#
+# @app.route('/diary_1', methods=['POST'])
+# def write_diary():
+#     first_receive = request.form['first']
+#     print(first_receive)
+#
+#     review = {
+#         'name': 'first',
+#         'content': first_receive
+#     }
+#
+#     db.reviews.insert_one(review)
+#     return jsonify({'result': 'success', 'msg': '저장되었습니다.'})
+
+
+
+
